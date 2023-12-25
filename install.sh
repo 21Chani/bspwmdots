a45ede0f2cd47dc520fe56b26cd5f0f4c213830f
21Chani <dmendoncacosta03@gmail.com>
2023-12-24 21:32:22 -0300

    install system packages

--- install.sh
+++ install.sh
@@ -11,8 +11,16 @@ bash "$relative_path/src/bin/yay_install.sh"
 echo "[INFO] Installing Xorg Server..."
 yay -S xorg xorg-xinit
 
+# Bspwm
 echo "[INFO] Installing bspwm..."
 cat "$relative_path/src/bspwm.packages.conf" | xargs yay -S
+
+# Extra packages
+cat "$relative_path/src/system.packages.conf" | xargs yay -S
+
+# Configure apps
+
+bash "$relative_path/src/bin/install_code_extensions.sh"
 
 #  ▄▄  ▄▄  ▄  ▄ ▄▄▄ ▄ ▄▄▄
 # █   █  █ ██▄█ █▄▄ █ █ ▄▄
