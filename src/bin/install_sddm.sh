8f47aa42f13430e3f579996f6162f0347f776064
21Chani <dmendoncacosta03@gmail.com>
2023-12-25 07:17:22 -0300

    enable and loggings for sddm

--- src/bin/install_sddm.sh
+++ src/bin/install_sddm.sh
@@ -1,16 +1,21 @@
 #/bin/sh!
 
 exampledir=$(
-    cd "$(dirname "$0/..")/example"
+    cd "$(dirname "$0")/../example"
     pwd
 )
 
 # Install SDDM
 echo "[INFO] Installing SDDM..."
-yay -S sddm-git
+yay -S --noconfirm sddm-git
 
 # Install Theme
 echo "[INFO] Installing SDDM Theme (Sugar Candy)... "
-yay -S sddm-sugar-candy-git
+yay -S --noconfirm sddm-sugar-candy-git
 
+echo "[INFO] Copying SDDM config..."
 sudo cp -r "$exampledir/sddm.conf" /etc/sddm.conf
+
+echo "[INFO] Enabling SDDM..."
+sudo systemctl enable sddm.service
+sudo systemctl start sddm.service
