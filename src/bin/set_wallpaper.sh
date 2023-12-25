fcd207142a494651eaf554b95e66d83ea57a05ff
21Chani <dmendoncacosta03@gmail.com>
2023-12-25 07:18:02 -0300

    nitrogen --save

--- src/bin/set_wallpaper.sh
+++ src/bin/set_wallpaper.sh
@@ -4,5 +4,5 @@ monitors=$(xrandr --listmonitors)
 
 xrandr --listmonitors | awk '/Monitors/{next} {print $NF}' | awk '{print NR-1, $0}' | while read -r index monitor; do
     echo "Setting background for Monitor $index: $monitor"
-    nitrogen --set-zoom-fill --head="$index" $HOME/.local/share/backgrounds/night_forest.jpg
+    nitrogen --set-zoom-fill --head="$index" $HOME/.local/share/backgrounds/night_forest.jpg --save
 done
