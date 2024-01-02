cd25100d4748cd59bac578bc3970d8009f2d0f9d
21Chani <dmendoncacosta03@gmail.com>
2024-01-02 16:18:35 -0300

    firefox helper

---
+++ src/bin/firefox_config.sh
@@ -0,0 +1,17 @@
+#/bin/sh!
+
+# Apps array
+apps=(
+    "github"
+    "whatsapp-web"
+    "vercel"
+    "instagram"
+    "youtube"
+)
+
+for app in "${apps[@]}"; do
+
+    # Open page for installation:
+    firefox -new-tab "https://github.com/catppuccin/userstyles/raw/main/styles/${app}/catppuccin.user.css"
+
+done
