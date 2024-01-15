1f5dd4faf3a5e0726d5651d7da5780812125871b
21Chani <dmendoncacosta03@gmail.com>
2024-01-15 08:25:03 -0300

    feat: betterdiscordctl install

--- install.sh
+++ install.sh
@@ -19,13 +19,14 @@ echo "[INFO] Installing bspwm..."
 yay -S --noconfirm - <"$relative_path/src/bspwm.packages.conf"
 
 # Extra packages
+echo "[INFO] Installing app packages..."
 yay -S --noconfirm - <"$relative_path/src/system.packages.conf"
 
 # Install zsh
 bash "$scripts_dir/zsh_install.sh"
 
 # Configure apps
-
+betterdiscordctl install
 sh "$scripts_dir/install_code_extensions.sh"
 
 #  ▄▄  ▄▄  ▄  ▄ ▄▄▄ ▄ ▄▄▄
